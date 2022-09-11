aea6440dcca91080ec0abb071ca10c1eb60163e8
ijuyong <ijuyong@gmail.com>
2022-09-11 19:05:19 +0900

    책등록 test 완료

---
+++ src/main/java/com/ijuyong/junitproject/web/dto/BookSaveReqDto.java
@@ -0,0 +1,18 @@
+package com.ijuyong.junitproject.web.dto;
+
+import com.ijuyong.junitproject.domain.Book;
+
+import lombok.Setter;
+
+@Setter // controller에서 setter가 호출되면서 dto에 값이 채워짐
+public class BookSaveReqDto {
+    private String title;
+    private String author;
+
+    public Book toEntity(){
+        return Book.builder()
+            .title(title)
+            .author(author)
+            .build();
+    }
+}
