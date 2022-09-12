9507d687be91da013fab1f052defbde4fb40a8a2
ijuyong <ijuyong@gmail.com>
2022-09-12 16:05:15 +0900

    책서비스 책등록완료

--- src/main/java/com/ijuyong/junitproject/web/dto/BookRespDto.java
+++ src/main/java/com/ijuyong/junitproject/web/dto/BookRespDto.java
@@ -1,5 +1,16 @@
 package com.ijuyong.junitproject.web.dto;
 
+import com.ijuyong.junitproject.domain.Book;
+
 public class BookRespDto {
-    
+    private long id;
+    private String title;
+    private String author;
+
+    public BookRespDto toDto(Book bookPS){
+        this.id = bookPS.getId();
+        this.title = bookPS.getTitle();
+        this.author = bookPS.getAuthor();
+        return this;
+    }
 }
